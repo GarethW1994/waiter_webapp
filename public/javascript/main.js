bee3cb1f69063667e8eee076b86252f6b2675167
GarethW1994 <williams.gareth0@gmail.com>
2017-07-06 14:12:23 +0200

    adding form validations

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,4 +1,5 @@
 var signUpForm = document.querySelector('.form-horizontal');
+var loginForm = document.querySelector('.form-inline');
 
 //get sign-up form DOM elements
 var userName = document.getElementById('name');
@@ -8,28 +9,51 @@ var password = document.getElementById('password');
 
 //sign-up form error messages
 var errorMsgSignUp = document.getElementById('errorMessSignUp');
+//login form error messages
+var errorMsgLogin = document.getElementById('errorMessLogin');
+
+//get login form DOM elements
+var userName2 = document.getElementById('username2');
+var password2 = document.getElementById('password2');
 
 
-//three loading dots
-var dotOne = document.querySelector('.dot1');
+//test function to validate the forms
+function test(signUpForm, loginForm) {
+	if (this.signUpForm !== null) {
+		this.signUpForm.addEventListener('change', function () {
+			var signupValues = [{
+				userName: userName.value,
+				userSurname: userSurname.value,
+				username: username.value,
+				password: password.value
+			}];
 
-userName.addEventListener('change', function() {
-	dotOne.classList.add('active');
-});
-
-signUpForm.addEventListener('change', function() {
+			//call validation function
+			//pass in validation parameters
+			var inputsSignup = validations(signupValues).validInputs();
 	
-	var signupValues = [{
-		userName: userName.value,
-		userSurname: userSurname.value,
-		username: username.value,
-		password: password.value
-	}];
+			//display outputs from validation functions
+			errorMsgSignUp.innerText = inputsSignup;
+		});
+		
+	}
 	
-	//call validation function
-	//pass in validation parameters
-	var inputs = validations(signupValues).validInputs();
-	
-	//display outputs from validation functions
-	errorMsgSignUp.innerText = inputs; 
-});
+	if (this.loginForm !== null) {
+		this.loginForm.addEventListener('change', function () {
+			var loginValues = [{
+				username: userName2.value,
+				password: password2.value
+			}];
+
+			//call validation function
+			//pass in validation parameters
+			var inputsLogin = validations(loginValues).validInputs();
+
+			//display outputs from validation functions
+			errorMsgLogin.innerText = inputsLogin;
+		});
+	}
+}
+
+//call the test function
+test();
--- public/javascript/validations.js
+++ public/javascript/validations.js
@@ -1,9 +1,13 @@
 var validations = function(values) {
 	
 	const validInputs = function() {	
+	if (values[0].userName !== undefined) {
 		if (values[0].userName.length == 0) {
 			return 'Required Fields Cannot Be Empty.';
-		} else if (values[0].username.length == 0) {
+		}
+	}
+		
+	if (values[0].username.length == 0) {
 			return 'Required Fields Cannot Be Empty.';
 		} else if (values[0].password.length == 0) {
 			return 'Required Fields Cannot Be Empty.';
